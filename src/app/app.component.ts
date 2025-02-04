a2a50c6cdbaa1a4208ba7401b0b63ae17cef65b0
Damian <daba9@op.pl>
2025-02-04 23:22:47 +0100

    rotate test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pulawskiego';
+  imageIndex = 1;
+  totalImages = 36;
+  private intervalId: any;
+
+  rotateLeft() {
+    this.imageIndex--;
+    if (this.imageIndex < 1) {
+      this.imageIndex = this.totalImages;
+    }
+  }
+
+  rotateRight() {
+    this.imageIndex++;
+    if (this.imageIndex > this.totalImages) {
+      this.imageIndex = 1;
+    }
+  }
+
+  startRotation(direction: 'left' | 'right') {
+    this.stopRotation();
+    this.intervalId = setInterval(() => {
+      direction === 'left' ? this.rotateLeft() : this.rotateRight();
+    }, 50);
+  }
+
+  stopRotation() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
 }
